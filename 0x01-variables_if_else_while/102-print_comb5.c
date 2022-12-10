006f5764c6810e0acefb58278e4f165c2fc2eb0d
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-09 20:55:04 -0800

    com

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,14 +19,14 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j != 99
+				if (i != 98 || j != 99)
 				{
 						putchar(',');
 						putchar(' ');
 				}
 						}
 						}
-						}
+					}
 	putchar('\n');
 	return (0);
 }
