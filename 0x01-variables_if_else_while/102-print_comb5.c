c7edb7463ff34afce705c0187742b8aaaba2ad70
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-09 20:20:18 -0800

    combo 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,41 +6,27 @@
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
-	int l_d2;
-	int f_d2;
-	int c2
+	int i, j;
 
-	while (c <= 98)
+	for (i = 0; i < 100; i++)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c2 = 0;
-			while (c2 <= 99)
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
 			{
-				f_d2 = (c2 / 10 + '0');
-				l_d2 = (c2 % 10 + '0');
-
-				if  (c < c2)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99
 				{
-					putchar(f_d);
-					putchar(l_d);
-					putchar(' ');
-					putchar(f_d2);
-					putchar(l_d2);
-
-					if (c != 98)
-					{
 						putchar(',');
 						putchar(' ');
-					}
 				}
-			}
-			c2++;
-	}
-	c++;
+						}
+						}
+						}
 	putchar('\n');
 	return (0);
 }
