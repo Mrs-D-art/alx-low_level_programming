4a1d86ce6e23e8969955d115a8806b9285435df0
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-13 14:20:28 -0800

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * @t: interger
  * _abs - absolute value
+ * @t: the interger
  * Return: always 0
  */
 int _abs(int t)
 {
-	if (t >=0 )
+	if (t >= 0)
 	{
 		return (t);
 	}
