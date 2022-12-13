505eab13ac1e7dba2c8eee4e5a7bfcada6ab2248
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-13 10:51:40 -0800

    lowercaseLetters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - print alphabet
+ * print_alphabet - prints alphabets in lowercase
  */
 void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		_putchar(ch);
 
 		_putchar('\n');
+	}
 
 }
