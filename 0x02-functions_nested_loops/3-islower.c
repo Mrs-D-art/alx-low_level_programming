44b6386590b886377500928e3336c7a9fab9ec2e
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-13 12:14:05 -0800

    is lowercaseLetter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _islower - prints lowercase
+ * Return: 1 if c is lowercase
+ * Return: 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
