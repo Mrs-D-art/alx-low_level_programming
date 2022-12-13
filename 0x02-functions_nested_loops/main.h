2c8ae5c5fd30690df231b974e295dca710c3f5a8
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-13 09:41:44 -0800

    incude main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -8,4 +10,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
+void print_to_98(int n)
+	
+#endif;
