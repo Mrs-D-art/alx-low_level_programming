146a3ba92dd15f1583c7fee907d99e8f59425235
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-13 18:27:16 -0800

    numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 			printf("%d, ", n--);
 		}
 		printf("%d\n", n);
-	
+
 	}
 	else if (n <= 98)
 	{
@@ -28,5 +28,4 @@ void print_to_98(int n)
 	{
 		printf("%d", n);
 	}
-	printf("\n");
 }
