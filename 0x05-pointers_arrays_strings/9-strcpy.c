1484f549c0c2236295e529b37a40197d15931134
Ngeyen Dorcas <dorcasbonje@gmail.com>
2022-12-19 16:57:12 -0800

    copies string?

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * puts_half - Prints half of a string.
- * @str: The string to be printed.
+ * _strcpy - Copies a string pointed to by @src, including the
+ *           terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
  */
-void puts_half(char *str)
+char *_strcpy(char *dest, const char *src)
 {
-	int index = 0, len = 0, n;
+	int index = 0;
 
-	while (str[index++])
-		len++;
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
 
-	if ((len % 2) == 0)
-		n = len / 2;
-
-	else
-		n = (len + 1) / 2;
-
-	for (index = n; index < len; index++)
-		_putchar(str[index]);
-
-	_putchar('\n');
+	return (dest);
 }
